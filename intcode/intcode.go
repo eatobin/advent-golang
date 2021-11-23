049f2970ce307f11bd04158b6e104f773e89f326
Eric Tobin <eatobin@gmail.com>
2021-11-23 15:18:47 -0600

    pad5 works!!

--- intcode/intcode.go
+++ intcode/intcode.go
@@ -11,7 +11,7 @@ func CharToInt(char byte) byte {
 	return char - 48
 }
 
-func pad5(op int) map[byte]byte {
+func Pad5(op int) map[byte]byte {
 	keys := [5]byte{'a', 'b', 'c', 'd', 'e'}
 	instruction := make(map[byte]byte)
 	asString := fmt.Sprintf("%05d", op)
@@ -25,8 +25,11 @@ func pad5(op int) map[byte]byte {
 }
 
 func main() {
-	myMap := pad5(12345)
+	myMap := Pad5(12345)
 	for key, value := range myMap {
 		fmt.Printf("%c :: %d\n", key, value)
 	}
+	//fmt.Printf("\nInt = %d", CharToInt('j'))
+	fmt.Printf("\nInt = %d", CharToInt('0'))
+	fmt.Printf("\nInt = %d", CharToInt('9'))
 }
