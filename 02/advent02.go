451e53ee81ddf8ee77295d775a1900e785411267
Eric Tobin <eatobin@gmail.com>
2021-12-01 14:39:20 -0600

    done!

--- 02/advent02.go
+++ 02/advent02.go
@@ -126,21 +126,26 @@ func updatedMemory(memory Memory, noun int, verb int) Memory {
 	return memory
 }
 
-func nounVerb() {
-	for noun := 0; noun < 101; noun++ {
-		for verb := 0; verb < 101; verb++ {
+func nounVerb() int {
+	var noun int
+	var verb int
+
+out:
+	for noun = 0; noun < 101; noun++ {
+		for verb = 0; verb < 101; verb++ {
 			tv := MakeMemory(fp)
 			candidate := opCode(IntCode{pointer: 0, memory: updatedMemory(tv, noun, verb)}).memory[0]
 			if candidate == 19690720 {
-				fmt.Printf("Part B answer = %d", (100*noun)+verb)
+				break out
 			}
 		}
 	}
+	return (100 * noun) + verb
 }
 
 func main() {
 	tv := MakeMemory(fp)
 	answer := opCode(IntCode{pointer: 0, memory: updatedMemory(tv, 12, 2)})
 	fmt.Printf("Part A answer = %d\n", answer.memory[0]) // Part A answer = 2890696
-	nounVerb()                                           // Part B answer = 8226
+	fmt.Printf("Part B answer = %d", nounVerb())         // Part B answer = 8226
 }
