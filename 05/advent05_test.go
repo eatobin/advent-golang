ff88a90ae6fe3b2742e5e1bb14d3bb3ea3481ddc
Eric Tobin <eatobin@gmail.com>
2021-12-11 17:40:03 -0600

    day05 testing progress

--- 05/advent05_test.go
+++ 05/advent05_test.go
@@ -19,6 +19,8 @@ func TestOpCode(t *testing.T) {
 			IntCode{input: 0, output: 0, pointer: 4, memory: []int{1101, 100, -1, 4, 99}}},
 		{IntCode{input: 8, output: 0, pointer: 0, memory: []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}},
 			IntCode{input: 8, output: 1, pointer: 8, memory: []int{3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8}}},
+		{IntCode{input: 7, output: 0, pointer: 0, memory: []int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}},
+			IntCode{input: 7, output: 1, pointer: 8, memory: []int{3, 9, 7, 9, 10, 9, 4, 9, 99, 1, 8}}},
 	}
 	for _, fixture := range fixtures {
 		value := OpCode(fixture.Value)
