d4c3e62bc92cc36e19045954028475c3b52b3ca6
Eric Tobin <eatobin@gmail.com>
2021-12-12 20:29:16 -0600

    day 05 test progress

--- 05/advent05_test.go
+++ 05/advent05_test.go
@@ -20,12 +20,15 @@ func TestOpCode(t *testing.T) {
 		// can be negative
 		{IntCode{input: 0, output: 0, pointer: 0, memory: []int{1101, 100, -1, 4, 0}},
 			IntCode{input: 0, output: 0, pointer: 4, memory: []int{1101, 100, -1, 4, 99}}},
-		// equal to 8
+		// equal to 8 - position
 		{IntCode{input: 8, output: 0, pointer: 0, memory: []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}},
 			IntCode{input: 8, output: 1, pointer: 8, memory: []int{3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8}}},
-		// less than 8
+		// less than 8 - position
 		{IntCode{input: 7, output: 0, pointer: 0, memory: []int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}},
 			IntCode{input: 7, output: 1, pointer: 8, memory: []int{3, 9, 7, 9, 10, 9, 4, 9, 99, 1, 8}}},
+		// equal to 8 - immediate
+		{IntCode{input: 8, output: 0, pointer: 0, memory: []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}},
+			IntCode{input: 8, output: 1, pointer: 8, memory: []int{3, 3, 1108, 1, 8, 3, 4, 3, 99}}},
 	}
 	for _, fixture := range fixtures {
 		value := OpCode(fixture.Value)
