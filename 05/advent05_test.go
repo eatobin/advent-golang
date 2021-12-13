5285ad6371b42572e6ae52836307bd42ff7cc32f
Eric Tobin <eatobin@gmail.com>
2021-12-12 21:16:29 -0600

    day 05 test progress done

--- 05/advent05_test.go
+++ 05/advent05_test.go
@@ -38,9 +38,12 @@ func TestOpCode(t *testing.T) {
 		// equal to 8 - immediate - fail
 		{IntCode{input: 88, output: 0, pointer: 0, memory: []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}},
 			IntCode{input: 88, output: 0, pointer: 8, memory: []int{3, 3, 1108, 0, 8, 3, 4, 3, 99}}},
-		// less than 8 - immediate
+		// less than 8 - immediate - pass
 		{IntCode{input: 7, output: 0, pointer: 0, memory: []int{3, 3, 1107, -1, 8, 3, 4, 3, 99}},
 			IntCode{input: 7, output: 1, pointer: 8, memory: []int{3, 3, 1107, 1, 8, 3, 4, 3, 99}}},
+		// less than 8 - immediate - fail
+		{IntCode{input: 77, output: 0, pointer: 0, memory: []int{3, 3, 1107, -1, 8, 3, 4, 3, 99}},
+			IntCode{input: 77, output: 0, pointer: 8, memory: []int{3, 3, 1107, 0, 8, 3, 4, 3, 99}}},
 		// jump on 0 - position
 		{IntCode{input: 0, output: 0, pointer: 0, memory: []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}},
 			IntCode{input: 0, output: 0, pointer: 11, memory: []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 0, 0, 1, 9}}},
