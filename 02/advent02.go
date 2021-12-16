4cee1eddf82ce346fa16ad67c2d46449f7300b97
Eric Tobin <eatobin@gmail.com>
2021-12-16 16:24:37 -0600

    day05 testing progress

--- 02/advent02.go
+++ 02/advent02.go
@@ -87,61 +87,61 @@ func getOrElse(pointer int, offsetX int, memory Memory) int {
 	}
 }
 
-func (ic IntCode) aParam(instruction Instruction) int {
+func (icP *IntCode) aParam(instruction Instruction) int {
 	var choice int
 	switch instruction['a'] {
 	// a-p-w
 	case 0:
-		choice = ic.memory[ic.pointer+offsetA]
+		choice = icP.memory[icP.pointer+offsetA]
 	}
 	return choice
 }
 
-func (ic IntCode) bParam(instruction Instruction) int {
+func (icP *IntCode) bParam(instruction Instruction) int {
 	var choice int
 	switch instruction['b'] {
 	// b-p-r
 	case 0:
-		choice = getOrElse(ic.pointer, offsetB, ic.memory)
+		choice = getOrElse(icP.pointer, offsetB, icP.memory)
 	}
 	return choice
 }
 
-func (ic IntCode) cParam(instruction Instruction) int {
+func (icP *IntCode) cParam(instruction Instruction) int {
 	var choice int
 	switch instruction['c'] {
 	// c-p-r
 	case 0:
-		choice = getOrElse(ic.pointer, offsetC, ic.memory)
+		choice = getOrElse(icP.pointer, offsetC, icP.memory)
 	}
 	return choice
 }
 
-func updateMemory(memory Memory, index int, value int) Memory {
-	memory[index] = value
-	return memory
-}
+//func updateMemory(memory Memory, index int, value int) Memory {
+//	memory[index] = value
+//	return memory
+//}
 
-func OpCode(ic IntCode) IntCode {
-	instruction := pad5(ic.memory[ic.pointer])
+func OpCode(icP *IntCode) int {
+	instruction := pad5(icP.memory[icP.pointer])
 	if instruction['d'] == 9 {
-		return ic
+		return 0
 	} else {
 		switch instruction['e'] {
 		case 1:
-			return OpCode(IntCode{
-				pointer: ic.pointer + 4,
-				memory: updateMemory(ic.memory,
-					ic.aParam(instruction),
-					ic.bParam(instruction)+ic.cParam(instruction)),
-			})
+			a := icP.aParam(instruction)
+			b := icP.bParam(instruction)
+			c := icP.cParam(instruction)
+			icP.memory[a] = b + c
+			icP.pointer += 4
+			return 1
 		case 2:
-			return OpCode(IntCode{
-				pointer: ic.pointer + 4,
-				memory: updateMemory(ic.memory,
-					ic.aParam(instruction),
-					ic.bParam(instruction)*ic.cParam(instruction)),
-			})
+			a := icP.aParam(instruction)
+			b := icP.bParam(instruction)
+			c := icP.cParam(instruction)
+			icP.memory[a] = b * c
+			icP.pointer += 4
+			return 1
 		default:
 			panic("opcode is not valid")
 		}
@@ -154,26 +154,34 @@ func updatedMemory(memory Memory, noun int, verb int) Memory {
 	return memory
 }
 
-func nounVerb() int {
-	var noun int
-	var verb int
-
-out:
-	for noun = 0; noun < 101; noun++ {
-		for verb = 0; verb < 101; verb++ {
-			tv := MakeMemory(fp)
-			candidate := OpCode(IntCode{pointer: 0, memory: updatedMemory(tv, noun, verb)}).memory[0]
-			if candidate == 19690720 {
-				break out
-			}
-		}
-	}
-	return (100 * noun) + verb
-}
+//func nounVerb() int {
+//	var noun int
+//	var verb int
+//
+//out:
+//	for noun = 0; noun < 101; noun++ {
+//		for verb = 0; verb < 101; verb++ {
+//			tv := MakeMemory(fp)
+//			candidate := OpCode(IntCode{pointer: 0, memory: updatedMemory(tv, noun, verb)}).memory[0]
+//			if candidate == 19690720 {
+//				break out
+//			}
+//		}
+//	}
+//	return (100 * noun) + verb
+//}
 
 func main() {
 	tv := MakeMemory(fp)
-	answer := OpCode(IntCode{pointer: 0, memory: updatedMemory(tv, 12, 2)})
-	fmt.Printf("Part A answer = %d\n", answer.memory[0]) // Part A answer = 2890696
-	fmt.Printf("Part B answer = %d", nounVerb())         // Part B answer = 8226
+	icP := &IntCode{
+		pointer: 0,
+		memory:  updatedMemory(tv, 12, 2),
+	}
+	icReturn := 1
+	for icReturn == 1 {
+		icReturn = OpCode(icP)
+	}
+	//answer := OpCode(IntCode{pointer: 0, memory: updatedMemory(tv, 12, 2)})
+	fmt.Printf("Part A answer = %d\n", icP.memory[0]) // Part A answer = 2890696
+	//fmt.Printf("Part B answer = %d", nounVerb())         // Part B answer = 8226
 }
