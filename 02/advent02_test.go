8dfee7a56da40de25e08e0cc11be1a41077c2057
Eric Tobin <eatobin@gmail.com>
2021-12-29 10:56:15 -0600

    02 works and tests!

--- 02/advent02_test.go
+++ 02/advent02_test.go
@@ -15,8 +15,8 @@ func TestOpCode(t *testing.T) {
 		{IntCode{pointer: 0, memory: map[int]int{0: 2, 1: 3, 2: 0, 3: 3, 4: 99}}, IntCode{pointer: 4, memory: map[int]int{0: 2, 1: 3, 2: 0, 3: 6, 4: 99}}},
 		{IntCode{pointer: 0, memory: map[int]int{0: 2, 1: 4, 2: 4, 3: 5, 4: 99, 5: 0}}, IntCode{pointer: 4, memory: map[int]int{0: 2, 1: 4, 2: 4, 3: 5, 4: 99, 5: 9801}}},
 		{IntCode{pointer: 0, memory: map[int]int{0: 1, 1: 1, 2: 1, 3: 4, 4: 99, 5: 5, 6: 6, 7: 0, 8: 99}}, IntCode{pointer: 8, memory: map[int]int{0: 30, 1: 1, 2: 1, 3: 4, 4: 2, 5: 5, 6: 6, 7: 0, 8: 99}}},
-		//{IntCode{pointer: 0, memory: []int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}},
-		//	IntCode{pointer: 8, memory: []int{3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50}}},
+		{IntCode{pointer: 0, memory: map[int]int{0: 1, 1: 9, 2: 10, 3: 3, 4: 2, 5: 3, 6: 11, 7: 0, 8: 99, 9: 30, 10: 40, 11: 50}},
+			IntCode{pointer: 8, memory: map[int]int{0: 3500, 1: 9, 2: 10, 3: 70, 4: 2, 5: 3, 6: 11, 7: 0, 8: 99, 9: 30, 10: 40, 11: 50}}},
 	}
 
 	for _, fixture := range fixtures {
