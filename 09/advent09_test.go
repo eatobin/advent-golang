5607b73096ececf1d77270b992434b95a8f5476b
Eric Tobin <eatobin@gmail.com>
2021-12-30 12:51:08 -0600

    start 09 test progress - all pass!!!

--- 09/advent09_test.go
+++ 09/advent09_test.go
@@ -14,22 +14,13 @@ func TestOpCode(t *testing.T) {
 	test00Out := MakeMemory("test00Out.csv")
 	test01In := MakeMemory("test01In.csv")
 	test01Out := MakeMemory("test01Out.csv")
-	//test02In := MakeMemory("test02In.csv")
-	//test02Out := MakeMemory("test02Out.csv")
-	//test03In := MakeMemory("test03In.csv")
-	//test03Out := MakeMemory("test03Out.csv")
-	//test04In := MakeMemory("test04In.csv")
-	//test04Out := MakeMemory("test04Out.csv")
-	//test05In := MakeMemory("test05In.csv")
-	//test05Out := MakeMemory("test05Out.csv")
+	test02In := MakeMemory("test02In.csv")
+	test02Out := MakeMemory("test02Out.csv")
 
 	fixtures := []Fixtures{
 		{IntCode{memory: test00In}, IntCode{pointer: 4, output: 109, relativeBase: 1, memory: test00Out}},
 		{IntCode{memory: test01In}, IntCode{pointer: 6, output: 1219070632396864, memory: test01Out}},
-		//{IntCode{pointer: 0, memory: test02In}, IntCode{pointer: 4, memory: test02Out}},
-		//{IntCode{pointer: 0, memory: test03In}, IntCode{pointer: 8, memory: test03Out}},
-		//{IntCode{pointer: 0, memory: test04In}, IntCode{pointer: 4, memory: test04Out}},
-		//{IntCode{pointer: 0, memory: test05In}, IntCode{pointer: 8, memory: test05Out}},
+		{IntCode{memory: test02In}, IntCode{output: 1125899906842624, pointer: 2, memory: test02Out}},
 	}
 
 	for _, fixture := range fixtures {
