36640fd84fe6dd364a87dacef6600b281a68cef8
Eric Tobin <eatobin@gmail.com>
2021-12-31 16:46:04 -0600

    5 progress

---
+++ 05/advent05.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/eatobin/advent-golang/intcode"
+)
+
+const fp = "05/advent05.csv"
+
+func main() {
+	tv := intcode.MakeMemory(fp)
+	icP := &intcode.IntCode{
+		Input:        1,
+		Output:       0,
+		Phase:        -1,
+		Pointer:      0,
+		RelativeBase: 0,
+		Memory:       tv,
+		IsStopped:    false,
+		DoesRecur:    true,
+	}
+	icReturn := 1
+	for icReturn == 1 {
+		icReturn = icP.OpCode()
+	}
+	fmt.Printf("Part A answer = %d\n", icP.Output) // Part A answer = 9025675
+
+	tv = intcode.MakeMemory(fp)
+	icP = &intcode.IntCode{
+		Input:        5,
+		Output:       0,
+		Phase:        -1,
+		Pointer:      0,
+		RelativeBase: 0,
+		Memory:       tv,
+		IsStopped:    false,
+		DoesRecur:    true,
+	}
+	icReturn = 1
+	for icReturn == 1 {
+		icReturn = icP.OpCode()
+	}
+	fmt.Printf("Part B answer = %d", icP.Output) // Part B answer = 11981754
+}
