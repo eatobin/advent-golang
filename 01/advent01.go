36f84b15d4c5091ec6ae6d2855a1836ab6e42849
Eric Tobin <eatobin@gmail.com>
2022-01-19 17:29:29 -0600

    01 progress

--- 01/advent01.go
+++ 01/advent01.go
@@ -18,20 +18,55 @@ func MakeModules(fp string) Modules {
 	}
 
 	txt := string(dat)
-	txt = strings.TrimRight(txt, "\n")
-	strOps := strings.Split(txt, "\n")
-	modules := make([]int, len(strOps))
-
-	for i, strOp := range strOps {
-		op, err := strconv.Atoi(strOp)
+	lines := strings.Split(txt, "\n")
+	strValues := lines[:len(lines)-1]
+	modules := make([]int, len(strValues))
+	for i, str := range strValues {
+		mass, err := strconv.Atoi(str)
 		if err != nil {
 			panic(err)
 		}
-		modules[i] = op
+		modules[i] = mass
 	}
 	return modules
 }
 
+//	fuel := Fuel(mass)
+//	sum += fuel
+//}
+//fmt.Println(sum)
+//dat, err := ioutil.ReadFile(fp)
+//if err != nil {
+//	panic(err)
+//}
+//
+//txt := string(dat)
+//txt = strings.TrimRight(txt, "\n")
+//strOps := strings.Split(txt, "\n")
+//modules := make([]int, len(strOps))
+//
+//for i, strOp := range strOps {
+//	op, err := strconv.Atoi(strOp)
+//	if err != nil {
+//		panic(err)
+//	}
+//	modules[i] = op
+//}
+//return modules
+//}
+
+// part a
+
+func Fuel(m int) int {
+	return (m / 3) - 2
+}
+
 func main() {
-	fmt.Printf("%v", MakeModules(fp))
+	sum := 0
+	ms := MakeModules(fp)
+	for _, v := range ms {
+		fuel := Fuel(v)
+		sum += fuel
+	}
+	fmt.Printf("Part A: %d", sum) // 3337766
 }
