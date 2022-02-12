76aeeff30b0ed66a99798a393fecb51abe60a846
Eric Tobin <eatobin@gmail.com>
2022-02-11 21:46:18 -0800

    makepath works + file read

--- 03/advent03.go
+++ 03/advent03.go
@@ -100,8 +100,52 @@ func makePath(move string, start []int) [][]int {
 	return path
 }
 
-func main() {
+//func main() {
+//
+//	sliceOfSlices := makePath("L3", []int{1, 1})
+//	fmt.Println("Slice of slices: ", sliceOfSlices)
+//}
 
-	sliceOfSlices := makePath("D3", []int{1, 1})
-	fmt.Println("Slice of slices: ", sliceOfSlices)
+var red []string
+var blue []string
+
+func main() {
+	both := make([][]string, 2)
+	// open file
+	f, err := os.Open("03/day03a.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// remember to close the file at the end of the program
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+		}
+	}(f)
+
+	// read csv values using csv.Reader
+	csvReader := csv.NewReader(f)
+	i := 0
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		// do something with read line
+		both[i] = rec
+		i++
+	}
+
+	red = both[0]
+	blue = both[1]
+
+	fmt.Printf("%+v\n", red)
+	fmt.Printf("%+v\n", blue)
+
+	sliceOfSlices := makePath("L3", []int{1, 1})
+	fmt.Println("\nSlice of slices: ", sliceOfSlices)
 }
