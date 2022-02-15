e09a9cc3a8d7ef6cf3f20933d1725b6db558d3fb
Eric Tobin <eatobin@gmail.com>
2022-02-15 15:18:11 -0800

    progress

--- 03/advent03.go
+++ 03/advent03.go
@@ -9,6 +9,12 @@ import (
 	"strconv"
 )
 
+type visit struct {
+	x, y int
+}
+type path = []visit
+type route = []path
+
 var fp = "03/day03a.csv"
 
 func MakeBoth(fp string) [][]string {
@@ -55,66 +61,54 @@ func distance(unit string) int {
 	return dist
 }
 
-func makePath(move string, start []int) [][]int {
+func makePath(move string, start visit) path {
 	direction := []byte(direction(move))[0]
 	distance := distance(move)
-	xStart := start[0]
-	yStart := start[1]
-	path := make([][]int, distance+1)
+	xStart := start.x
+	yStart := start.y
+	path := make(path, distance+1)
 
 	switch direction {
 	case 'R':
 		for i := 0; i < distance+1; i++ {
-			path[i] = make([]int, 2)
-		}
-		for i := 0; i < distance+1; i++ {
-			path[i] = []int{xStart, yStart}
+			path[i] = visit{xStart, yStart}
 			xStart++
 		}
 	case 'U':
 		for i := 0; i < distance+1; i++ {
-			path[i] = make([]int, 2)
-		}
-		for i := 0; i < distance+1; i++ {
-			path[i] = []int{xStart, yStart}
+			path[i] = visit{xStart, yStart}
 			yStart++
 		}
 	case 'L':
 		for i := 0; i < distance+1; i++ {
-			path[i] = make([]int, 2)
-		}
-		for i := 0; i < distance+1; i++ {
-			path[i] = []int{xStart, yStart}
+			path[i] = visit{xStart, yStart}
 			xStart--
 		}
 	case 'D':
 		for i := 0; i < distance+1; i++ {
-			path[i] = make([]int, 2)
-		}
-		for i := 0; i < distance+1; i++ {
-			path[i] = []int{xStart, yStart}
+			path[i] = visit{xStart, yStart}
 			yStart--
 		}
 	}
-
 	return path
 }
 
-func makePaths(start []int, moves []string) [][]int {
-	var flatPaths [][]int
-	paths := make([][][]int, len(moves))
+func makeRoute(start visit, moves []string) route {
+	//	var flatPaths [][]int
+	route := make(route, len(moves))
+	//	paths := make([][][]int, len(moves))
 	pathStart := start
 	for i, move := range moves {
 		path := makePath(move, pathStart)
-		paths[i] = path
+		route[i] = path
 		pathStart = path[len(path)-1]
 	}
-	for _, path := range paths {
-		for _, move := range path {
-			flatPaths = append(flatPaths, move)
-		}
-	}
-	return flatPaths
+	//	for _, path := range paths {
+	//		for _, move := range path {
+	//			flatPaths = append(flatPaths, move)
+	//		}
+	//	}
+	return route
 }
 
 var red []string
@@ -157,15 +151,15 @@ func main() {
 	fmt.Printf("%+v\n", red)
 	fmt.Printf("%+v\n", blue)
 
-	sliceOfSlices := makePath("L3", []int{1, 1})
+	sliceOfSlices := makePath("D3", visit{x: 0, y: 1})
 	fmt.Println("\nSlice of slices: ", sliceOfSlices)
 
-	paths := makePaths([]int{0, 0}, red)
+	paths := makeRoute(visit{x: 0, y: 0}, red)
 	fmt.Println("\nPaths: ", paths)
 
-	fmt.Println("\nPathsA: ", paths[0])
-	fmt.Println("\nPathsB: ", paths[0][0])
-	//fmt.Println("\nPathsC: ", paths[0][0][0])
+	//fmt.Println("\nPathsA: ", paths[0])
+	//fmt.Println("\nPathsB: ", paths[0][0])
+	////fmt.Println("\nPathsC: ", paths[0][0][0])
 }
 
 //type visit struct {
