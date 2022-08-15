eaa90252a79fa3e43a8a71eec0c916e07f3a464f
eatobin <eatobin@gmail.com>
2022-08-14 21:58:37 -0500

    return to forever

--- 01/advent01.go
+++ 01/advent01.go
@@ -1,48 +1,74 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+    "fmt"
+    "io/ioutil"
+    "strconv"
+    "strings"
 )
 
-type Modules = []int
+/*
 
-const fp = "01/advent01.txt"
+Fuel required to launch a given module is based on its mass. Specifically, to
+find the fuel required for a module, take its mass, divide by three, round
+down, and subtract 2.
 
-func MakeModules(fp string) Modules {
-	dat, err := os.ReadFile(fp)
-	if err != nil {
-		panic(err)
-	}
-
-	txt := string(dat)
-	txt = strings.TrimRight(txt, "\n")
-	strOps := strings.Split(txt, "\n")
-	modules := make([]int, len(strOps))
-
-	for i, strOp := range strOps {
-		op, err := strconv.Atoi(strOp)
-		if err != nil {
-			panic(err)
-		}
-		modules[i] = op
-	}
-	return modules
+*/
+func Fuel(mass int) int {
+    return (mass / 3) - 2
 }
 
-// part a
+/*
 
-func Fuel(m int) int {
-	return (m / 3) - 2
+Fuel2 calculate its fuel and add it to the total. Then, treat the fuel amount you
+just calculated as the input mass and repeat the process, continuing until a
+fuel requirement is zero or negative.
+
+*/
+func Fuel2(mass int) int {
+    sum := 0
+    for {
+        fuel := Fuel(mass)
+        if fuel <= 0 {
+            return sum
+        }
+        sum += fuel
+
+        mass = Fuel(fuel)
+        if mass <= 0 {
+            return sum
+        }
+        sum += mass
+    }
 }
 
 func main() {
-	sum := 0
-	ms := MakeModules(fp)
-	for _, v := range ms {
-		sum += Fuel(v)
-	}
-	fmt.Printf("Part A: %d", sum) // 3337766
+    dat, err := ioutil.ReadFile("01/advent01.txt")
+    if err != nil {
+        panic(err)
+    }
+    txt := string(dat)
+    fields := strings.Fields(txt)
+
+    sum := 0
+    for _, value := range fields {
+        mass, err := strconv.Atoi(value)
+        if err != nil {
+            panic(err)
+        }
+        fuel := Fuel(mass)
+        sum += fuel
+    }
+    fmt.Printf("Part a: %d\n", sum) // 3337766
+
+    sum = 0
+    for _, value := range fields {
+        mass, err := strconv.Atoi(value)
+        if err != nil {
+            panic(err)
+        }
+        fuel := Fuel2(mass)
+        sum += fuel
+    }
+    fmt.Printf("Part b: %d\n", sum)
 }
