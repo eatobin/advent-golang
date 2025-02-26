fac0dc3cc96d3bff9e58b57f31e4b4bab3f66475
Eric Tobin <eatobin@gmail.com>
2025-02-25 17:17:59 -0700

    progress

--- 05/advent05.go
+++ 05/advent05.go
@@ -1,44 +1,80 @@
 package main
 
 import (
-	"advent-golang/intCodePkg"
 	"fmt"
 )
 
-const fp = "advent05.csv"
+// Instruction:
+// ABCDE
+// 01234
+// 01002
+// 34 - two-digit opcode,      02 == opcode 2
+//  2 - mode of 1st parameter,  0 == position mode
+//  1 - mode of 2nd parameter,  1 == immediate mode
+//  0 - mode of 3rd parameter,  0 == position mode,
+//                                   omitted due to being a leading zero
+// 0 1 or 2 = left-to-right position after 2 digit opcode
+// p i or r = position, immediate or relative mode
+// r or w = read or write
+
+type Intcode struct {
+	input   int
+	output  int
+	pointer int
+	memory  [11]int
+}
+
+type Instruction []int
 
 func main() {
-	tv := intCodePkg.MakeMemory(fp)
-	icP := &intCodePkg.IntCode{
-		Input:        1,
-		Output:       []int{},
-		Phase:        -1,
-		Pointer:      0,
-		RelativeBase: 0,
-		Memory:       tv,
-		IsStopped:    false,
-		DoesRecur:    true,
-	}
-	icReturn := 1
-	for icReturn == 1 {
-		icReturn = icP.OpCode()
-	}
-	fmt.Printf("Part A answer = %d\n", icP.Output[len(icP.Output)-1]) // Part A answer = 9025675
+	var intcode Intcode
+	//var icReturn int
 
-	tv = intCodePkg.MakeMemory(fp)
-	icP = &intCodePkg.IntCode{
-		Input:        5,
-		Output:       []int{},
-		Phase:        -1,
-		Pointer:      0,
-		RelativeBase: 0,
-		Memory:       tv,
-		IsStopped:    false,
-		DoesRecur:    true,
-	}
-	icReturn = 1
-	for icReturn == 1 {
-		icReturn = icP.OpCode()
-	}
-	fmt.Printf("Part B answer = %d\n", icP.Output[len(icP.Output)-1]) // Part B answer = 11981754
+	intcode = makeIntcode()
+	//icReturn = 1
+	//
+	//for icReturn == 1 {
+	//	icReturn = opcode(&intcode)
+	//}
+
+	fmt.Printf("\nPart A answer = %d. Correct = 2890696\n", intcode.memory[0])
 }
+
+func makeIntcode() Intcode {
+	intcode := Intcode{
+		input:   0,
+		output:  0,
+		pointer: 0,
+		memory:  [11]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8},
+	}
+	return intcode
+}
+
+//func opcode(intCode *Intcode) int {
+//	var action int
+//	var address1 int
+//	var address2 int
+//	var address3 int
+//
+//	action = intCode.memory[intCode.pointer]
+//	address1 = intCode.memory[intCode.pointer+1]
+//	address2 = intCode.memory[intCode.pointer+2]
+//	address3 = intCode.memory[intCode.pointer+3]
+//
+//	switch action {
+//	case 1:
+//		intCode.memory[address3] =
+//			intCode.memory[address1] +
+//				intCode.memory[address2]
+//		intCode.pointer += 4
+//		return 1
+//	case 2:
+//		intCode.memory[address3] =
+//			intCode.memory[address1] *
+//				intCode.memory[address2]
+//		intCode.pointer += 4
+//		return 1
+//	default:
+//		return 0
+//	}
+//}
