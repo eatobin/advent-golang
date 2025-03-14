44fa7018a29772cd27f5ffa8ac979b62a9e64f44
Eric Tobin <eatobin@gmail.com>
2025-03-13 20:56:06 -0700

    works3

--- 07/advent07.go
+++ 07/advent07.go
@@ -1,24 +1,27 @@
+// Does this version correct:
+// https://en.wikipedia.org/wiki/Heap%27s_algorithm#Frequent_mis-implementations
+
 package main
 
 import "fmt"
 
-func HeapPermutation(a []int, size int) {
-	if size == 1 {
-		fmt.Println(a)
+func permutations(k int, A []int) {
+	if k == 1 {
+		fmt.Println(A)
 	} else {
-		for i := 0; i < size-1; i++ {
-			HeapPermutation(a, size-1)
-			if size%2 == 0 {
-				a[i], a[size-1] = a[size-1], a[i]
+		permutations(k-1, A)
+		for i := 0; i < k-1; i++ {
+			if k%2 == 0 {
+				A[i], A[k-1] = A[k-1], A[i]
 			} else {
-				a[0], a[size-1] = a[size-1], a[0]
+				A[0], A[k-1] = A[k-1], A[0]
 			}
+			permutations(k-1, A)
 		}
-		HeapPermutation(a, size-1)
 	}
 }
 
 func main() {
-	a := []int{1, 2, 3}
-	HeapPermutation(a, len(a))
+	A := []int{1, 2, 3, 4, 5}
+	permutations(len(A), A)
 }
