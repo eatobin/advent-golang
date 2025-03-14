af9414191ffdb932111b1e4f2dde5e19e25c21b1
Eric Tobin <eatobin@gmail.com>
2025-03-13 20:33:41 -0700

    works2

--- 07/advent07.go
+++ 07/advent07.go
@@ -6,15 +6,15 @@ func HeapPermutation(a []int, size int) {
 	if size == 1 {
 		fmt.Println(a)
 	} else {
-		HeapPermutation(a, size-1)
 		for i := 0; i < size-1; i++ {
+			HeapPermutation(a, size-1)
 			if size%2 == 0 {
 				a[i], a[size-1] = a[size-1], a[i]
 			} else {
 				a[0], a[size-1] = a[size-1], a[0]
 			}
-			HeapPermutation(a, size-1)
 		}
+		HeapPermutation(a, size-1)
 	}
 }
 
