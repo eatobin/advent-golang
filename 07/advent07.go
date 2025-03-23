e1897627dccd481f8246fa5d03ef6067e36fe5cf
Eric Tobin <eatobin@gmail.com>
2025-03-22 20:45:33 -0700

    works

--- 07/advent07.go
+++ 07/advent07.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 // Instruction:
@@ -24,7 +25,7 @@ type IntCode struct {
 	output    int
 	phase     int
 	pointer   int
-	memory    [678]int
+	memory    [17]int
 	isStopped bool
 	doesRecur bool
 }
@@ -33,7 +34,7 @@ const offsetC int = 1
 const offsetB int = 2
 const offsetA int = 3
 
-var memoryConstant = [678]int{3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 79, 14, 225, 1101, 17, 42, 225, 2, 74, 69, 224, 1001, 224, -5733, 224, 4, 224, 1002, 223, 8, 223, 101, 4, 224, 224, 1, 223, 224, 223, 1002, 191, 83, 224, 1001, 224, -2407, 224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1, 223, 224, 223, 1101, 18, 64, 225, 1102, 63, 22, 225, 1101, 31, 91, 225, 1001, 65, 26, 224, 101, -44, 224, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 224, 223, 223, 101, 78, 13, 224, 101, -157, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 3, 224, 1, 224, 223, 223, 102, 87, 187, 224, 101, -4698, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 4, 224, 1, 223, 224, 223, 1102, 79, 85, 224, 101, -6715, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 2, 224, 1, 224, 223, 223, 1101, 43, 46, 224, 101, -89, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 1, 224, 224, 1, 223, 224, 223, 1101, 54, 12, 225, 1102, 29, 54, 225, 1, 17, 217, 224, 101, -37, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224, 1, 223, 224, 223, 1102, 20, 53, 225, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 107, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 329, 101, 1, 223, 223, 1108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 344, 101, 1, 223, 223, 7, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 359, 101, 1, 223, 223, 108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 374, 101, 1, 223, 223, 8, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 389, 101, 1, 223, 223, 1108, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 404, 101, 1, 223, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 419, 101, 1, 223, 223, 8, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 434, 1001, 223, 1, 223, 1008, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 449, 1001, 223, 1, 223, 1008, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 464, 101, 1, 223, 223, 1107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 479, 101, 1, 223, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 494, 1001, 223, 1, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 509, 101, 1, 223, 223, 1108, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 524, 101, 1, 223, 223, 7, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 539, 101, 1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 554, 101, 1, 223, 223, 8, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 569, 1001, 223, 1, 223, 1008, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 584, 101, 1, 223, 223, 107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 599, 1001, 223, 1, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 614, 101, 1, 223, 223, 1007, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 629, 101, 1, 223, 223, 1107, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 644, 101, 1, 223, 223, 108, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 659, 101, 1, 223, 223, 1007, 677, 226, 224, 102, 2, 223, 223, 1006, 224, 674, 101, 1, 223, 223, 4, 223, 99, 226}
+var memoryConstant = [17]int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0}
 
 func makeIntcode() IntCode {
 	intcode := IntCode{
@@ -120,7 +121,7 @@ func permutations(k int, A []int) {
 	}
 }
 
-func pass(candidate []int, commonMemory [678]int, instruction *[5]int) int {
+func pass(candidate []int, commonMemory [17]int, instruction *[5]int) int {
 	memA := commonMemory
 	memB := commonMemory
 	memC := commonMemory
@@ -211,102 +212,102 @@ func pass(candidate []int, commonMemory [678]int, instruction *[5]int) int {
 	return icpE.output
 }
 
-func pass2(candidate []int, commonMemory [678]int, instruction *[5]int) int {
-	memA := commonMemory
-	memB := commonMemory
-	memC := commonMemory
-	memD := commonMemory
-	memE := commonMemory
-	//for key, value := range commonMemory {
-	//	memA[key] = value
-	//	memB[key] = value
-	//	memC[key] = value
-	//	memD[key] = value
-	//	memE[key] = value
-	//}
-	eOutput := 0
-	allStopped := false
-	icpA := &IntCode{
-		input:     0,
-		output:    0,
-		phase:     candidate[0],
-		pointer:   0,
-		memory:    memA,
-		isStopped: false,
-		doesRecur: false,
-	}
-	icpB := &IntCode{
-		input:     0,
-		output:    0,
-		phase:     candidate[1],
-		pointer:   0,
-		memory:    memB,
-		isStopped: false,
-		doesRecur: false,
-	}
-	icpC := &IntCode{
-		input:     0,
-		output:    0,
-		phase:     candidate[2],
-		pointer:   0,
-		memory:    memC,
-		isStopped: false,
-		doesRecur: false,
-	}
-	icpD := &IntCode{
-		input:     0,
-		output:    0,
-		phase:     candidate[3],
-		pointer:   0,
-		memory:    memD,
-		isStopped: false,
-		doesRecur: false,
-	}
-	icpE := &IntCode{
-		input:     0,
-		output:    0,
-		phase:     candidate[4],
-		pointer:   0,
-		memory:    memE,
-		isStopped: false,
-		doesRecur: false,
-	}
+//func pass2(candidate []int, commonMemory [17]int, instruction *[5]int) int {
+//	memA := commonMemory
+//	memB := commonMemory
+//	memC := commonMemory
+//	memD := commonMemory
+//	memE := commonMemory
+//	//for key, value := range commonMemory {
+//	//	memA[key] = value
+//	//	memB[key] = value
+//	//	memC[key] = value
+//	//	memD[key] = value
+//	//	memE[key] = value
+//	//}
+//	eOutput := 0
+//	allStopped := false
+//	icpA := &IntCode{
+//		input:     0,
+//		output:    0,
+//		phase:     candidate[0],
+//		pointer:   0,
+//		memory:    memA,
+//		isStopped: false,
+//		doesRecur: false,
+//	}
+//	icpB := &IntCode{
+//		input:     0,
+//		output:    0,
+//		phase:     candidate[1],
+//		pointer:   0,
+//		memory:    memB,
+//		isStopped: false,
+//		doesRecur: false,
+//	}
+//	icpC := &IntCode{
+//		input:     0,
+//		output:    0,
+//		phase:     candidate[2],
+//		pointer:   0,
+//		memory:    memC,
+//		isStopped: false,
+//		doesRecur: false,
+//	}
+//	icpD := &IntCode{
+//		input:     0,
+//		output:    0,
+//		phase:     candidate[3],
+//		pointer:   0,
+//		memory:    memD,
+//		isStopped: false,
+//		doesRecur: false,
+//	}
+//	icpE := &IntCode{
+//		input:     0,
+//		output:    0,
+//		phase:     candidate[4],
+//		pointer:   0,
+//		memory:    memE,
+//		isStopped: false,
+//		doesRecur: false,
+//	}
+//
+//	for !allStopped {
+//		icReturn := 1
+//		for icReturn == 1 {
+//			icReturn = opcode(icpA, instruction)
+//		}
+//		icpB.input = icpA.output
+//		icReturn = 1
+//		for icReturn == 1 {
+//			icReturn = opcode(icpB, instruction)
+//		}
+//		icpC.input = icpB.output
+//		icReturn = 1
+//		for icReturn == 1 {
+//			icReturn = opcode(icpC, instruction)
+//		}
+//		icpD.input = icpC.output
+//		icReturn = 1
+//		for icReturn == 1 {
+//			icReturn = opcode(icpD, instruction)
+//		}
+//		icpE.input = icpD.output
+//		icReturn = 1
+//		for icReturn == 1 {
+//			icReturn = opcode(icpE, instruction)
+//		}
+//
+//		icpA.input = icpE.output
+//		eOutput = icpE.output
+//		allStopped = icpE.isStopped
+//	}
+//
+//	return eOutput
+//}
 
-	for !allStopped {
-		icReturn := 1
-		for icReturn == 1 {
-			icReturn = opcode(icpA, instruction)
-		}
-		icpB.input = icpA.output
-		icReturn = 1
-		for icReturn == 1 {
-			icReturn = opcode(icpB, instruction)
-		}
-		icpC.input = icpB.output
-		icReturn = 1
-		for icReturn == 1 {
-			icReturn = opcode(icpC, instruction)
-		}
-		icpD.input = icpC.output
-		icReturn = 1
-		for icReturn == 1 {
-			icReturn = opcode(icpD, instruction)
-		}
-		icpE.input = icpD.output
-		icReturn = 1
-		for icReturn == 1 {
-			icReturn = opcode(icpE, instruction)
-		}
-
-		icpA.input = icpE.output
-		eOutput = icpE.output
-		allStopped = icpE.isStopped
-	}
-
-	return eOutput
-}
-
-func passes(candidates [][]int, memory [678]int, instruction *[5]int) []int {
+func passes(memory [17]int, instruction *[5]int) []int {
 	vcm := make([]int, len(candidates))
 	for i, v := range candidates {
 		vcm[i] = pass(v, memory, instruction)
@@ -314,13 +315,13 @@ func passes(candidates [][]int, memory [678]int, instruction *[5]int) []int {
 	return vcm
 }
 
-func passes2(candidates [][]int, memory [678]int, instruction *[5]int) []int {
-	vcm := make([]int, len(candidates))
-	for i, v := range candidates {
-		vcm[i] = pass2(v, memory, instruction)
-	}
-	return vcm
-}
+//func passes2(candidates [][]int, memory [17]int, instruction *[5]int) []int {
+//	vcm := make([]int, len(candidates))
+//	for i, v := range candidates {
+//		vcm[i] = pass2(v, memory, instruction)
+//	}
+//	return vcm
+//}
 
 func opcode(icP *IntCode, instruction *[5]int) int {
 	if icP.isStopped {
@@ -417,14 +418,10 @@ func opcode(icP *IntCode, instruction *[5]int) int {
 //}
 
 func main() {
+	A := []int{0, 1, 2, 3, 4}
+	permutations(len(A), A)
 	instruction := [5]int{}
-	intcode := makeIntcode()
-	var icReturn = 1
-
-	for icReturn == 1 {
-		icReturn = opcode(&intcode, &instruction)
-	}
-
-	fmt.Printf("\nPart A answer = %d. Correct = 9025675\n", intcode.output)
-
+	answer := passes(memoryConstant, &instruction)
+	sort.Ints(answer)
+	fmt.Printf("Part A answer = %d. Correct = 368584\n", answer[len(answer)-1])
 }
